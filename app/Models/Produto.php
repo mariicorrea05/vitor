7da2a48eb4e2dda3b21076428bbc5f9966fd3a0a
Marina Corrêa <marinacorreas56@gmail.com>
2025-05-27 21:35:57 -0300

    feat-adiciona config do banco de dados

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nome', 'preco', 'tipo_produto_id', 'fornecedor_id'];
+
+    public function tipoProduto()
+    {
+        return $this->belongsTo(TipoProduto::class, 'tipo_produto_id');
+    }
+
+    public function fornecedor()
+    {
+        return $this->belongsTo(Fornecedor::class, 'fornecedor_id');
+    }
 }
